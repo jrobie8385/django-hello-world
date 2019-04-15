a5b72fa7bc750a248ede632f65456194f206f5b7
jrobie8385 <jrobie8385@yahoo.com>
2019-04-14 17:03:59 -0700

    Commit for class-based views and redirection page

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "home"
 
 urlpatterns = [
-    path("", views.homePageView, name = "home")
+    path("", views.homePageView, name = "home"),
+    path("nextPage/", views.newPage, name = "next"),
+    path("anotherPage/", views.AnotherPage.as_view(), name = "another"),
+    path("redirectionPage/", views.RedirectPage.as_view(), name = "redirect")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
 def homePageView(request):
     return HttpResponse("Testing this page...")
+
+def newPage(request):
+    return HttpResponse("This page helps to test typing in the words 'nextPage/' into the URL after the base server.")
+
+class AnotherPage(TemplateView):
+    template_name = "pages/yet_another_page.html"
+
+class RedirectPage(TemplateView):
+    template_name = "pages/redirection_page.html"
